66ead185ab20d2fc6d40491d96331fefe688cf21
MCurb <marcoscurbeco03@gmail.com>
2025-07-07 17:20:32 -0600

    Add the basic logic to add books through a form

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
 const container = document.querySelector(".container")
 
-const myLibrary = [
-    {title: "Alter Ego", author: "Todd Herman"},
-    {title: "Can't hurt me", author: "David Goggins"}
-]
+// Gather all book objects
+const myLibrary = [];
+
 
 function Book(title, author) {
     this.title = title;
     this.author = author;
 }
 
+// Put them in the DOM
 Book.prototype.displayBook = function() {
     const title = document.createElement("p");
     const author = document.createElement("p");
@@ -19,11 +19,26 @@ Book.prototype.displayBook = function() {
     author.textContent = this.author;
 }
 
-for (const book of myLibrary) {
-    const libro = new Book(book.title, book.author)
+// Create book objects and add them to the array
+function addBookToLibrary(title, author) {
+    myLibrary.push({title: title, author: author})
+    displayBooks();
+}
+
+// Select the last book object from the array and create a new book object with the book constructor
+function displayBooks() {
+    lastBook = myLibrary[myLibrary.length - 1];
+    const libro = new Book(lastBook.title, lastBook.author)
     libro.displayBook();
 }
 
-// function addBookToLibrary() {
-
-// }
+// Take the form data to create a new book object
+const bookForm = document.querySelector(".book-form")
+const titleInput = document.querySelector(".title-input")
+const authorInput = document.querySelector(".author-input")
+bookForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    addBookToLibrary(titleInput.value, authorInput.value)
+    console.log(myLibrary)
+})
