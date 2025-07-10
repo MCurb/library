e6463bb3ed58718ed5bd7f5f3195edd583715ea9
MCurb <marcoscurbeco03@gmail.com>
2025-07-10 15:44:20 -0600

    Add all the main logic of the project

--- javascript.js
+++ javascript.js
@@ -3,13 +3,14 @@ const container = document.querySelector(".main-content");
 // Gather all book objects
 const myLibrary = [];
 
-function Book(title, author, pages, importance, note, status) {
+function Book(title, author, pages, importance, note, status, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.importance = importance;
   this.note = note;
   this.status = status;
+  this.id = id;
 }
 
 const table = document.querySelector(".books-table");
@@ -17,34 +18,70 @@ const table = document.querySelector(".books-table");
 // Put them in the DOM
 Book.prototype.displayBook = function () {
   const newRow = document.createElement("tr");
+  newRow.setAttribute("data-book-id", `${this.id}`);
   table.appendChild(newRow);
   const title = document.createElement("td");
   const author = document.createElement("td");
   const pages = document.createElement("td");
   const importance = document.createElement("td");
   const note = document.createElement("td");
-  const status = document.createElement("td");
+
+  const statusCell = document.createElement("td");
+  const statusBtn = document.createElement("button");
+  statusBtn.setAttribute("data-book-id", `${this.id}`);
+
+  statusBtn.addEventListener("click", () => {
+    if (statusBtn.textContent === "Read") {
+      statusBtn.textContent = "Not read";
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id === statusBtn.dataset.bookId) {
+          myLibrary[i].status = "Not read";
+        }
+      }
+    } else {
+      statusBtn.textContent = "Read";
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id === statusBtn.dataset.bookId) {
+          myLibrary[i].status = "Read";
+        }
+      }
+    }
+  });
+  statusCell.appendChild(statusBtn);
   const deleteCell = document.createElement("td");
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
+  deleteBtn.setAttribute("data-book-id", `${this.id}`);
   deleteCell.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", () => {
+    if (newRow.dataset.bookId === deleteBtn.dataset.bookId) {
+      newRow.remove();
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id === deleteBtn.dataset.bookId) {
+          myLibrary.splice(i, 1);
+        }
+      }
+    }
+  });
+
   newRow.appendChild(title);
   newRow.appendChild(author);
   newRow.appendChild(pages);
   newRow.appendChild(importance);
   newRow.appendChild(note);
-  newRow.appendChild(status);
+  newRow.appendChild(statusCell);
   newRow.appendChild(deleteCell);
   title.textContent = this.title;
   author.textContent = this.author;
   pages.textContent = this.pages;
   importance.textContent = this.importance;
   note.textContent = this.note;
-  status.textContent = this.status;
+  statusBtn.textContent = this.status;
 };
 
 // Create book objects and add them to the array
-function addBookToLibrary(title, author, pages, importance, note, status) {
+function addBookToLibrary(title, author, pages, importance, note, status, id) {
   myLibrary.push({
     title: title,
     author: author,
@@ -52,6 +89,7 @@ function addBookToLibrary(title, author, pages, importance, note, status) {
     importance: importance,
     note: note,
     status: status,
+    id: id,
   });
   displayBooks();
 }
@@ -65,7 +103,8 @@ function displayBooks() {
     lastBook.pages,
     lastBook.importance,
     lastBook.note,
-    lastBook.status
+    lastBook.status,
+    lastBook.id
   );
   libro.displayBook();
 }
@@ -87,7 +126,8 @@ bookForm.addEventListener("submit", function (event) {
     rangeInput.value,
     selectInput.value,
     textareaInput.value,
-    selectStatusInput.value
+    selectStatusInput.value,
+    self.crypto.randomUUID()
   );
   console.log(myLibrary);
 });
